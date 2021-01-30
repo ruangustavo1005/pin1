b3305d3a1b91e7f66aede64dd5d1061d3df6e279
ruanzada <ruan.gustavo.moretti10@gmail.com>
2021-01-30 16:45:32 -0300

    ini

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
     <head>
         <meta charset="windows-1252">
@@ -11,7 +6,9 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'teste';
+        $oTag = new Estrutura\Tag('div', 'teste');
+        $oTag->addAtributo('border', '1');
+        echo $oTag->getHtml();
         ?>
     </body>
 </html>
