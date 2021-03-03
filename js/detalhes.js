f77d0cbedc4ed88e7ee808b4746cd8fbd2a10122
leofu <leofu@DESKTOP-MCT759B>
2021-03-02 23:05:20 -0300

    ajustes pre agendamento

--- js/detalhes.js
+++ js/detalhes.js
@@ -19,16 +19,60 @@ function nextImage() {
 function prevImage() {
     var images = document.getElementsByClassName("img_slide"),
         max    = images.length;
-        
     images[currentImageIndex].classList.remove("selected");
-    
     currentImageIndex--;
-
     if (currentImageIndex < 0) {
         currentImageIndex = max - 1;
     }
-    
     images[currentImageIndex].classList.add("selected");
 }
 
-//window.addEventListener("load", start)
+function selectHorario(oHorario) {
+    var oTd      = document.getElementById(oHorario.id),
+       oSelected = document.querySelector('.selected-image');
+    if(oTd.innerText === 'Disponível' ) {
+        if(oSelected) {
+            oSelected.classList.remove('selected-image');
+            oSelected.classList.add('selecionavel');
+        }
+        oTd.removeAttribute('class');
+        oTd.classList.add('selected-image');
+    }
+    
+}
+
+function realizaAgendamento() { 
+    debugger;
+    var oSelected = document.querySelector('.selected-image'),
+        sPlaca = document.querySelector('#placa').innerText,
+        oNome = document.getElementById('nome'),
+        oCpf = document.getElementById('cpf'),
+        oData = document.getElementById('nascimento'),
+        oEmail = document.getElementById('email'),
+        oTelefone = document.getElementById('telefone');
+
+    if (oSelected && sPlaca) {
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost/pin1/estrutura/ajax.php',
+            data: {
+                controller: 'ControllerAgendamento',
+                processo: 'realizaAgendamento',
+                placa: sPlaca,
+                horario: oSelected.id,
+                nome: oNome.value,
+                cpf: oCpf.value,
+                nascimento: oData.value,
+                email: oEmail.value,
+                telefone: oTelefone.value
+            },
+            cache: false,
+            dataType: 'html',
+            success: function (oRes) {
+                if (oRes) {
+                    $("body").html(oRes);
+                }
+            }
+        });
+    }
+}
--- teste.php
+++ teste.php
@@ -1,9 +1,9 @@
 <?php
 
-//$today = time();
-//$wday = date('w', $today);   
-//echo date('d/m/Y', $today - ($wday - 1)*86400) . '<br>';
-//echo date('d/m/Y', $today - ($wday - 2)*86400) . '<br>';
-//echo date('d/m/Y', $today - ($wday - 3)*86400) . '<br>';
-//echo date('d/m/Y', $today - ($wday - 4)*86400) . '<br>';
-//echo date('d/m/Y', $today - ($wday - 5)*86400) . '<br>';
+$today = time();
+$wday = date('w', $today);   
+echo date('d/m/Y', $today - ($wday - 1)*86400) . '<br>';
+echo date('d/m/Y', $today - ($wday - 2)*86400) . '<br>';
+echo date('d/m/Y', $today - ($wday - 3)*86400) . '<br>';
+echo date('d/m/Y', $today - ($wday - 4)*86400) . '<br>';
+echo date('d/m/Y', $today - ($wday - 5)*86400) . '<br>';
